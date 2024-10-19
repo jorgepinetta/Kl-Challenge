609dcefe576361dda7b66d5623a3bf8c5f546c2f
JP <jorgep@puertadigital.com>
2024-10-19 08:22:42 -0300

    - r0.6 - Crear Tests - Ejecucion de tests (ORIGINALES Y NUEVOS)

---
+++ DevelopmentChallenge.Data.Tests/DataEvoTestsNew.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using DevelopmentChallenge.Data.Contracts;
+using DevelopmentChallenge.Data.Models;
+using DevelopmentChallenge.Data.Services;
+using NUnit.Framework;
+
+namespace DevelopmentChallenge.Data.Tests
+{
+  [TestFixture]
+  public class DataEvoTestsNew
+  {
+    [TestCase]
+    public void TestResumeEmptyList()
+    {
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "es"), Is.EqualTo("<h1>Lista vacía de formas!</h1>"));
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "en"), Is.EqualTo("<h1>Empty list of shapes!</h1>"));
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "it"), Is.EqualTo("<h1>Elenco vuoto di forme!</h1>"));
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "ru"), Is.EqualTo("<h1>[empty-list]</h1>"));
+    }
+
+    [TestCase]
+    public void TestResumenMultiShapes()
+    {
+      var formas = new List<IShape>
+      {
+        new SquareShape(5),
+        new CircleShape(3),
+        new EquilateralTriangleShape(4),
+        new RectangleShape(2, 4),
+        new SquareShape(2),
+        new EquilateralTriangleShape(9),
+        new TrapezoidShape(4, 2, 5, 8, 5),
+        new CircleShape(2.75m),
+        new EquilateralTriangleShape(4.2m),
+      };
+
+      var resumen = ShapeReportService.HtmlReport(formas, "es");
+      Assert.That(resumen, Is.EqualTo("<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>1 Rectángulo | Area 8 | Perimetro 12 <br/>1 Trapezoide | Area 15 | Perimetro 19 <br/>TOTAL:<br/>9 formas Perimetro 128,66 Area 114,65"));
+
+      resumen = ShapeReportService.HtmlReport(formas, "en");
+      Assert.That(resumen, Is.EqualTo("<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>1 Rectangle | Area 8 | Perimeter 12 <br/>1 Trapezoid | Area 15 | Perimeter 19 <br/>TOTAL:<br/>9 shapes Perimeter 128,66 Area 114,65"));
+
+      resumen = ShapeReportService.HtmlReport(formas, "it");
+      Assert.That(resumen, Is.EqualTo("<h1>Rapporto sulle forme</h1>2 Quadrati | Area 29 | Perimetro 28 <br/>2 Cerchi | Area 13,01 | Perimetro 18,06 <br/>3 Triangoli | Area 49,64 | Perimetro 51,6 <br/>1 Rettangolo | Area 8 | Perimetro 12 <br/>1 Trapezio | Area 15 | Perimetro 19 <br/>TOTALE:<br/>9 forme Perimetro 128,66 Area 114,65"));
+    }
+  }
+}
---
+++ DevelopmentChallenge.Data.Tests/DataEvoTestsOriginals.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using DevelopmentChallenge.Data.Contracts;
+using DevelopmentChallenge.Data.Models;
+using DevelopmentChallenge.Data.Services;
+using NUnit.Framework;
+
+namespace DevelopmentChallenge.Data.Tests
+{
+  [TestFixture]
+  public class DataEvoTestsOriginals
+  {
+    [TestCase]
+    public void TestResumenListaVacia()
+    {
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "es"), Is.EqualTo("<h1>Lista vacía de formas!</h1>"));
+    }
+
+    [TestCase]
+    public void TestResumenListaVaciaFormasEnIngles()
+    {
+      Assert.That(ShapeReportService.HtmlReport(new List<IShape>(), "en"), Is.EqualTo("<h1>Empty list of shapes!</h1>"));
+    }
+
+    [TestCase]
+    public void TestResumenListaConUnCuadrado()
+    {
+      var cuadrados = new List<IShape> { new SquareShape(5) };
+
+      var resumen = ShapeReportService.HtmlReport(cuadrados, "es");
+
+      Assert.That(resumen, Is.EqualTo("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25"));
+    }
+
+    [TestCase]
+    public void TestResumenListaConMasCuadrados()
+    {
+      var cuadrados = new List<IShape>
+      {
+        new SquareShape(5),
+        new SquareShape(1),
+        new SquareShape(3),
+      };
+
+      var resumen = ShapeReportService.HtmlReport(cuadrados, "en");
+
+      Assert.That(resumen, Is.EqualTo("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35"));
+    }
+
+    [TestCase]
+    public void TestResumenListaConMasTipos()
+    {
+      var formas = new List<IShape>
+      {
+        new SquareShape(5),
+        new CircleShape(3),
+        new EquilateralTriangleShape(4),
+        new SquareShape(2),
+        new EquilateralTriangleShape(9),
+        new CircleShape(2.75m),
+        new EquilateralTriangleShape(4.2m),
+      };
+
+      var resumen = ShapeReportService.HtmlReport(formas, "en");
+
+      Assert.That(resumen, Is.EqualTo("<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65"));
+    }
+
+    [TestCase]
+    public void TestResumenListaConMasTiposEnCastellano()
+    {
+      var formas = new List<IShape>
+      {
+        new SquareShape(5),
+        new CircleShape(3),
+        new EquilateralTriangleShape(4),
+        new SquareShape(2),
+        new EquilateralTriangleShape(9),
+        new CircleShape(2.75m),
+        new EquilateralTriangleShape(4.2m),
+      };
+
+      var resumen = ShapeReportService.HtmlReport(formas, "es");
+
+      Assert.That(resumen, Is.EqualTo("<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65"));
+    }
+  }
+}
