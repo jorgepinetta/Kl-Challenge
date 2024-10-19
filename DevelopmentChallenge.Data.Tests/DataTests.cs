2b82fc64851000880031616bd45c93140207da8c
JP <jorgep@puertadigital.com>
2024-10-19 06:35:38 -0300

    - Update nuget - r0.2

--- DevelopmentChallenge.Data.Tests/DataTests.cs
+++ DevelopmentChallenge.Data.Tests/DataTests.cs
@@ -11,25 +11,23 @@ namespace DevelopmentChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaVacia()
         {
-            Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 1));
+            Assert.That(FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 1), Is.EqualTo("<h1>Lista vacía de formas!</h1>"));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
-            Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 2));
+            Assert.That(FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 2), Is.EqualTo("<h1>Empty list of shapes!</h1>"));
         }
 
         [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            var cuadrados = new List<FormaGeometrica> {new FormaGeometrica(FormaGeometrica.Cuadrado, 5)};
+            var cuadrados = new List<FormaGeometrica> { new FormaGeometrica(FormaGeometrica.Cuadrado, 5) };
 
             var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Castellano);
 
-            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
+            Assert.That(resumen, Is.EqualTo("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25"));
         }
 
         [TestCase]
@@ -44,7 +42,7 @@ namespace DevelopmentChallenge.Data.Tests
 
             var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Ingles);
 
-            Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
+            Assert.That(resumen, Is.EqualTo("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35"));
         }
 
         [TestCase]
@@ -63,9 +61,7 @@ namespace DevelopmentChallenge.Data.Tests
 
             var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Ingles);
 
-            Assert.AreEqual(
-                "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
-                resumen);
+            Assert.That(resumen, Is.EqualTo("<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65"));
         }
 
         [TestCase]
@@ -84,9 +80,7 @@ namespace DevelopmentChallenge.Data.Tests
 
             var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Castellano);
 
-            Assert.AreEqual(
-                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
-                resumen);
+            Assert.That(resumen, Is.EqualTo("<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65"));
         }
     }
 }
